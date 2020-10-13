d954d5d234505ec4e3d4d947036d25a24b65d34c
ukikagi <ukikagi@gmail.com>
2020-10-14 00:23:09 +0900

    first commit

---
+++ src/google_interview.lean
@@ -0,0 +1,17 @@
+universes u
+
+inductive mytree (A : Type u) : Type u
+| leaf : A → mytree
+| branch : A → mytree → mytree → mytree
+
+def flip_mytree {A : Type u} : mytree A → mytree A
+| t@(mytree.leaf _)     := t
+| (mytree.branch a l r) := mytree.branch a (flip_mytree r) (flip_mytree l)
+
+-- universe u
+
+theorem flip_flip {A : Type u} {t : mytree A} : flip_mytree (flip_mytree t) = t :=
+begin
+  induction t with _ a t1 t2 ih1 ih2; unfold flip_mytree,
+  rw [ih1, ih2],
+end
---
+++ src/real_fibonacci.lean
@@ -0,0 +1,41 @@
+import data.real.basic data.nat.fib
+import tactic
+
+noncomputable theory
+
+def phi : ℝ := ((1 + real.sqrt 5) / 2)
+
+def psi : ℝ := ((1 - real.sqrt 5) / 2)
+
+def SUBMISSION := ∀ n : ℕ, (nat.fib n : ℝ) = (phi^n - psi^n) / real.sqrt 5
+notation `SUBMISSION` := SUBMISSION
+
+lemma lt_wrap {n m: nat} {h: n < m}: n.lt m
+:= by assumption
+
+lemma ind2 {C: ℕ → Prop} {n: ℕ}:
+  C 0 → C 1 → (∀ k: ℕ, C k → C (k + 1) → C (k + 2)) → C n
+:=
+begin
+  intros h0 h1 hstep,
+  apply nat.lt_wf.induction,
+  intros n ih,
+  rcases n with _ | _ | n,
+    assumption',
+  apply hstep,
+  apply ih n (by apply lt_wrap; omega),
+  apply ih (n + 1) (by apply lt_wrap; omega),
+end
+
+theorem binet (n : ℕ) : (nat.fib n : ℝ) = (phi^n - psi^n) / real.sqrt 5 :=
+begin
+  induction n using ind2 with n ihn1 ihn2,
+    simp,
+    sorry,
+  rw nat.fib_succ_succ,
+    simp,
+    rw [ihn1, ihn2],
+    
+    
+
+end
